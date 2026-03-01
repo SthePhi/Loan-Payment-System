33310429f8bf7caf7e651c41c0dd50f81f86cd1e
SthePhi <masekosthembiso@gmail.com>
2026-03-01 17:12:09 +0200

    adding packages

---
+++ src/test/java/com/example/LoanPaymentApplication/LoanPaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LoanPaymentApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanPaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
